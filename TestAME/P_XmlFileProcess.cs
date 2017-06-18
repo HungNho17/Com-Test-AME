dbfaf7a9c020986f3077bbed3501917ded6d2dbe
THANHLONG <nphung75@gmail.com>
2017-06-18 18:03:32 +0700

    [Meanning Update] Commit for process manage Image on local folder. .Using XML to manage Image Information.

---
+++ TestAME/P_XmlFileProcess.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.IO;
+using System.Xml.Linq;
+
+namespace TestAME
+{
+    class P_XmlFileProcess
+    {
+        public struct ImageInfo
+        {
+            public string ImageName;
+            public string Group;
+            public string Desc;
+            public string Conte;
+        };
+
+        //==============================================================================
+        // Attributes.
+        //==============================================================================
+        #region ATTRIBUTES
+        string XMLFilePath = null;
+        bool FlagFileLoaded = false;
+
+        XmlDocument XMLFileCurr = null;
+        XmlNodeList XMLNodeListFile = null;
+
+        List<ImageInfo> ListOfImageInfo = null;
+        #endregion
+
+        //==============================================================================
+        // Operations -- API PUBLIC
+        //==============================================================================
+        #region API_PUBLIC
+        public P_XmlFileProcess(string iPathFile)
+        {
+            XMLFileCurr = new XmlDocument();
+            ListOfImageInfo = new List<ImageInfo>() { };
+            if (XML_FileCheck(iPathFile))
+            {
+                XMLNodeListFile = XMLFileCurr.SelectNodes("/Image");
+            }
+            XMLFilePath = iPathFile;
+            XMLFileCurr.Load(XMLFilePath);
+            FlagFileLoaded = true;
+        }
+
+        public List<ImageInfo> GetImageListInfo()
+        {
+            List<ImageInfo> listRet = null;
+
+            ImageInfo tempImageInfo = new ImageInfo();
+            XmlNodeList tempList;
+
+            XmlNode root = XMLFileCurr.SelectSingleNode("/ImageManage");
+            tempList = root.SelectNodes("Image");
+            foreach (XmlNode element in XMLNodeListFile)
+            {
+                tempImageInfo.ImageName = ((XmlElement)element).GetAttribute("name");
+                tempImageInfo.Group = ((XmlElement)((XmlElement)element).SelectSingleNode("Group")).GetAttribute("name");
+                tempImageInfo.Desc = ((XmlElement)((XmlElement)element).SelectSingleNode("Desc")).GetAttribute("name");
+                tempImageInfo.Conte = ((XmlElement)((XmlElement)element).SelectSingleNode("Conte")).GetAttribute("name");
+                ListOfImageInfo.Add(tempImageInfo);
+            }
+
+            if (ListOfImageInfo.Count > 0)
+            {
+                listRet = ListOfImageInfo;
+            }
+
+            return listRet;
+        }
+
+        public bool AddNewImage(string iImageName, string iGroup, string iImageDesc, string iImageConte)
+        {
+            bool bRet = true;
+            XMLFileCurr.Load(XMLFilePath);
+
+            XmlElement eImage = XMLFileCurr.CreateElement("Image", null);
+            eImage.SetAttribute("name", iImageName);
+
+            XmlElement eGroup = XMLFileCurr.CreateElement("Group", null);
+            eGroup.SetAttribute("name", iGroup);
+            eImage.AppendChild(eGroup);
+
+            XmlElement eImageDesc = XMLFileCurr.CreateElement("Desc", null);
+            eImageDesc.SetAttribute("name", iImageDesc);
+            eImage.AppendChild(eImageDesc);
+
+            XmlElement eImageConte = XMLFileCurr.CreateElement("Conte", null);
+            eImageConte.SetAttribute("name", iImageConte);
+            eImage.AppendChild(eImageConte);
+            
+            XMLFileCurr.DocumentElement.AppendChild(eImage);
+            XMLFileCurr.Save(XMLFilePath);
+
+            return bRet;
+        }
+
+        public bool RemoveImage(string iImageName)
+        {
+            bool bRet = false;
+            XMLFileCurr.Load(XMLFilePath);
+
+            XmlNode root = XMLFileCurr.SelectSingleNode ("/ImageManage");
+            XMLNodeListFile = root.SelectNodes("Image");
+            foreach (XmlNode element in XMLNodeListFile)
+            {
+                XmlElement temp = (XmlElement)element;
+                if (temp.GetAttribute("name") == iImageName)
+                {
+                    element.ParentNode.RemoveChild(element);
+                    XMLFileCurr.Save(XMLFilePath);
+                    bRet = true;
+                    break;
+                }
+            }
+
+            return bRet;
+        }
+        #endregion
+        //==============================================================================
+        // Operations -- API PRIVATE
+        //==============================================================================
+        #region API_PRIVATE
+        private bool XML_FileCheck(string iPathFile)
+        {
+            bool bRet = true;
+            if (File.Exists(iPathFile) == false)
+            {
+                XDocument doc = new XDocument(  new XElement("ImageManage", "\n\n\n\n"));
+                doc.Save(iPathFile);
+                bRet = false;
+            }
+            return bRet;
+        }
+        #endregion
+    }
+}
--- TestAME/SW_LabelsProgramming.cs
+++ TestAME/SW_LabelsProgramming.cs
@@ -6,15 +6,253 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Imaging;
+using System.Xml;
 
 namespace TestAME
 {
     public partial class SW_LabelsProgramming : Form
     {
+
+        //==============================================================================
+        // Atributes.
+        //==============================================================================
+        #region ATTRIBUTES
+
+        Size Image_Small = new Size(270, 90);
+        Size Image_Medium = new Size(360, 90);
+        Size Image_Normal = new Size(450, 90);
+
+        string ImageResourceName = "\\ImageResource";
+        string ImageResourcePath = null;
+
+        Bitmap ImageCurrent = null;
+        string ImageGroup = null;
+        string ImageDesc = null;
+        string ImageConte = null;
+
+        string XMLFileName = "\\ImageManage.XML";
+        P_XmlFileProcess ImageManageXML = null;
+        #endregion
+        //==============================================================================
+        // Operations -- WINDOW ACTIONS
+        //==============================================================================
+        #region WINDOW_ACTION
+
         public SW_LabelsProgramming()
         {
             InitializeComponent();
         }
+        private void SW_LabelsProgramming_Load(object sender, EventArgs e)
+        {
+            UpdateRightContextManu();
+            CheckImageResource();
 
+            ImageManageXML = new P_XmlFileProcess(ImageResourcePath+ XMLFileName);
+        }
+        #endregion
+        //==============================================================================
+        // Operations -- API ACTIONS
+        //==============================================================================
+        #region API_ACTIONS
+
+        #endregion
+        //==============================================================================
+        // Operations -- INTERNAL ACTIONS
+        //==============================================================================
+        #region INTERNAL_ACTIONS
+
+        private bool CheckImageResource()
+        {
+            bool bRet = false;
+            string DirectorPath = Directory.GetCurrentDirectory();
+            ImageResourcePath = DirectorPath + ImageResourceName;
+            if (Directory.Exists(ImageResourcePath))
+            {
+                bRet = true;
+            }
+            else
+            {
+                try
+                {
+                    Directory.CreateDirectory(ImageResourcePath);
+                    bRet = true;
+                }
+                catch
+                { }
+            }
+            return bRet;
+        }
+
+        private bool CheckResourceManage()
+        {
+            bool bRet = false;
+
+            return bRet;
+        }
+        private bool UpdateRightContextManu()
+        {
+            bool bRet = true;
+            
+            ContextMenu cm = new ContextMenu();
+            cm.MenuItems.Add("Copy", new EventHandler(CurrentPicture_copy));
+            cm.MenuItems.Add("Paste", new EventHandler(CurrentPicture_paste));
+            pbCurrentPicture.ContextMenu = cm; 
+
+            return bRet;
+        }
+        private void CurrentPicture_copy(Object s, EventArgs e)
+        {
+            if (pbCurrentPicture.Image != null)
+            {
+                Clipboard.SetImage(pbCurrentPicture.Image);
+            }
+        }
+        private void CurrentPicture_paste(Object s, EventArgs e)
+        {
+            if (Clipboard.GetImage() != null)
+            {
+                SetPicture(Clipboard.GetImage(), Image_Normal);
+            }
+        }
+        private bool SetPicture(Image iImage, Size iSize)
+        {
+            bool bRet = true;
+
+            pbCurrentPicture.Size = iSize;
+            pbCurrentPicture.Image = iImage;
+            pbCurrentPicture.Image = new Bitmap(pbCurrentPicture.Image, iSize);
+
+            int x = (pbCurrentPicture.Parent.ClientSize.Width / 2) - (pbCurrentPicture.Width / 2);
+            int y = (pbCurrentPicture.Parent.ClientSize.Height / 2) - (pbCurrentPicture.Height / 2);
+            pbCurrentPicture.Location = new Point(x,y);
+            pbCurrentPicture.Refresh();
+
+            return bRet;
+        }
+        private bool SavePicture(Image iImage, string iImageName)
+        {
+            bool bRet = false;
+            string sTempPath = ImageResourcePath + "\\" + iImageName;
+            Bitmap bTempImage = new Bitmap(iImage);
+            bTempImage.Save(sTempPath, ImageFormat.Png);
+            return bRet;
+        }
+        private Bitmap LoadPicture(string iImageName)
+        {
+            Bitmap ImageRet = null;
+
+            string sTempPath = ImageResourcePath + "\\" + iImageName;
+            if (File.Exists(sTempPath))
+            {
+                ImageRet = (Bitmap) Image.FromFile(sTempPath);
+            }
+
+            return ImageRet;
+        }
+        private bool RemovePicture(string iImageName)
+        {
+            bool bRet = false;
+            string sTempPath = ImageResourcePath + "\\" + iImageName;
+            if (File.Exists(sTempPath))
+            {
+                try
+                {
+                    File.Delete(sTempPath);
+                }
+                catch { }
+            }
+            return bRet;
+        }
+
+        #endregion
+        //==============================================================================
+        // Operations -- SUB-FORM PROCESS
+        //==============================================================================
+        #region SUB-FORM_PROCESS
+
+        #endregion
+        //==============================================================================
+        // Operations -- EVENT PROCESS
+        //==============================================================================
+        #region EVENT_PROCESS
+
+        private void tsmiSmall_Click(object sender, EventArgs e)
+        {
+            ToolStripMenuItem tempObject = sender as ToolStripMenuItem;
+            tsmiSmall.Checked = false;
+            tsmiMedium.Checked = false;
+            tsmiNormal.Checked = false;
+
+            if (tempObject == tsmiSmall)
+            {
+                tsmiSmall.Checked = true;
+                SetPicture(pbCurrentPicture.Image, Image_Small);
+            }
+            else if (tempObject == tsmiMedium)
+            {
+                tsmiMedium.Checked = true;
+                SetPicture(pbCurrentPicture.Image, Image_Medium);
+            }
+            else if (tempObject == tsmiNormal)
+            {
+                tsmiNormal.Checked = true;
+                SetPicture(pbCurrentPicture.Image, Image_Normal);
+            }
+        }
+
+        private void tsmiEditPicture_Click(object sender, EventArgs e)
+        {
+            ToolStripMenuItem tempObject = sender as ToolStripMenuItem;
+            tsmiEditEnable.Checked = false;
+            tsmiEditDisable.Checked = false;
+
+            if (tempObject == tsmiEditEnable)
+            {
+                tsmiEditEnable.Checked = true;
+                btAddImage.Enabled = true;
+                btDeleteImage.Enabled = true;
+                btClear.Enabled = true;
+            }
+            else if (tempObject == tsmiEditDisable)
+            {
+                tsmiEditDisable.Checked = true;
+                btAddImage.Enabled = false;
+                btDeleteImage.Enabled = false;
+                btClear.Enabled = false;
+            }
+        }
+        private void btClear_Click(object sender, EventArgs e)
+        {
+            Bitmap temp = LoadPicture("image_001");
+            pbCurrentPicture.Image = temp;
+        }
+
+        private void btAddImage_Click(object sender, EventArgs e)
+        {
+            if (pbCurrentPicture.Image != null)
+            {
+                if (cbGroup.Text == "" || cbDesc.Text == "" || cbCont.Text == "")
+                {
+                    MessageBox.Show("Please give enough information in _Group, _Desc, _Conte !");
+                }
+                else
+                {
+                    SavePicture(pbCurrentPicture.Image, "image_001");
+                    ImageManageXML.AddNewImage("image_001", cbGroup.Text, cbDesc.Text, cbCont.Text);
+                    MessageBox.Show("Add done !");
+                }
+            }
+        }
+
+        private void btDeleteImage_Click(object sender, EventArgs e)
+        {
+            RemovePicture("image_001");
+            ImageManageXML.RemoveImage ("image_001");
+        }
+
+
+        #endregion
     }
 }
